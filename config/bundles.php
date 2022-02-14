b650d103f69e918f119f53067cf35e3c652aa279
Henrique <Henrique Vasconcelos>
2022-02-14 20:38:10 -0300

    Configuracao de CORS

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
