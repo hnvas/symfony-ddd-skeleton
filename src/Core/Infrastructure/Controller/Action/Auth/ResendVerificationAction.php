ee499518bd6a7f0fb88ec89973cf8c51cf7f1b2c
Henrique <Henrique Vasconcelos>
2022-02-18 15:12:14 -0300

    Reenvio de email de verificacao

---
+++ src/Core/Infrastructure/Controller/Action/Auth/ResendVerificationAction.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Core\Infrastructure\Controller\Action\Auth;
+
+use App\Core\Application\Services\Mail\UserEmailVerification;
+use App\Core\Domain\Model\User;
+use App\Core\Infrastructure\Http\Response\ApiEmptyResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ResendVerificationAction
+ * @package App\Core\Infrastructure\Controller\Action\Auth
+ * @author  Henrique Vasconcelos <henriquenvasconcelos@gmail.com>
+ *
+ * @Route("/auth/resend", name="resend_verification_email", methods={"GET"})
+ */
+class ResendVerificationAction extends AbstractController
+{
+
+    private UserEmailVerification $emailVerification;
+
+    public function __construct(UserEmailVerification $emailVerification)
+    {
+        $this->emailVerification = $emailVerification;
+    }
+
+    /**
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function __invoke(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        /** @var User $user */
+        $this->getUser();
+
+        $this->emailVerification->send($user);
+
+        return new ApiEmptyResponse();
+    }
+}
