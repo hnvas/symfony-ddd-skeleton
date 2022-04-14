3ea65290c6bed542a52740b6fd73c267a6b2b5f5
Henrique Vasconcelos <henriquenvasconcelos@gmail.com>
2022-04-14 18:18:13 -0300

    Testes de unidade para o filtro de usuarios

---
+++ tests/Unit/Core/Infrastructure/Repository/Filter/UserFilterTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Tests\Unit\Core\Infrastructure\Repository\Filter;
+
+use App\Core\Domain\Model\User;
+use App\Core\Infrastructure\Repository\Filters\FilterFactory;
+use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\QueryBuilder;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class UserFilterTest extends TestCase
+{
+
+    private MockObject $queryBuilderMock;
+
+    public function setUp(): void
+    {
+        $this->queryBuilderMock = self::createMock(QueryBuilder::class);
+        $this->queryBuilderMock
+            ->method('getRootAliases')
+            ->willReturn(['e']);
+    }
+
+    public function testShouldApplyNameAndEmailFilter(): void
+    {
+        $expMock = self::createMock(Expr::class);
+        $expMock->expects(self::exactly(2))
+            ->method('like')
+            ->willReturnSelf();
+        $this->queryBuilderMock
+            ->expects(self::exactly(2))
+            ->method('expr')
+            ->willReturn($expMock);
+        $this->queryBuilderMock
+            ->expects(self::exactly(2))
+            ->method('andWhere')
+            ->willReturnSelf();
+        $this->queryBuilderMock
+            ->expects(self::exactly(2))
+            ->method('setParameter')
+            ->willReturnSelf();
+
+        $params = ['name' => 'Admin', 'email' => 'user@admin.com'];
+        $filter = FilterFactory::create(
+            $params,
+            $this->queryBuilderMock,
+            User::class
+        );
+
+        $filter->apply();
+    }
+}
